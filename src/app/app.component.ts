14b730bcdc57f5f41d170f3709f46828626b7278
hsv123456789 <harisrevatcha@gmail.com>
2024-11-12 14:02:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'pushNotificationPwa';
+
+
+  sendNotification() {
+    // Check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      // If permission is already granted, display the notification
+      this.showNotification();
+    } else if (Notification.permission !== "denied") {
+      // Ask the user for permission
+      Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+          this.showNotification();
+        }
+      });
+    }
+  }
+
+  showNotification() {
+    new Notification("Hello from the push notification application", {
+      body: "This is your desktop notification!",
+      icon: "assets/icons/icon-96x96.png", // Path to your app icon
+    });
+  }
 }
